b74ba83c223cf517ef5f19524e5f0503d1da00c0
Taivan Shelby <taivanalves7@gmail.com>
2021-01-31 22:56:53 -0300

    First commit

---
+++ src/paulomenu.js
@@ -0,0 +1,12 @@
+const paulomenu = (prefix, pushname) => {
+    return `◪ *Comandos do LA MÁFIA BOT*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.paulomenu = paulomenu
